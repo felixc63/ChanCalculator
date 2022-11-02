d629beabbf0128df4ba182571c44406b09291203
Felix Chan <felixc63@nycstudents.net>
2022-11-01 22:43:30 -0400

    Finished

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double number1, number2;
+    private String operation;
+
+    public Calculator(double number1, double number2, String operation){
+        this.number1 = number1;
+        this.number2 = number2;
+        this.operation = operation;
+    }
+
+    public double result(){
+        if (operation.equals("*")){
+            return number1 * number2;
+        } else if (operation.equals("/")) {
+            return number1/number2;
+        } else if (operation.equals("+")){
+            return number1 + number2;
+        } else if (operation.equals("-")){
+            return number1 - number2;
+        } else if (operation.equals("%")){
+            return number1%number2;
+        } else if (operation.equals("^")){
+            return Math.pow(number1,number2);
+        } else{
+            return 0;
+        }
+    }
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+import java.util.Scanner;
+
+public class CalculatorRunner {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
+        String expression = scan.nextLine();
+        String operation = "";
+
+        if (expression.contains("*")){
+            operation = "*";
+        } else if (expression.contains("/")){
+            operation = "/";
+        } else if (expression.contains("+")){
+            operation = "+";
+        } else if (expression.contains("-")){
+            operation = "-";
+        } else if (expression.contains("%")) {
+            operation = "%";
+        } else if (expression.contains("^")) {
+            operation = "^";
+        } else{
+            System.out.println("Please enter an accepted operation.");
+            System.exit(0);
+        }
+
+        double number1 = Double.parseDouble(expression.substring(0,expression.indexOf(operation)));
+        double number2 = Double.parseDouble(expression.substring(expression.indexOf(operation)+1));
+
+        Calculator calc = new Calculator(number1, number2, operation);
+        System.out.println("Result: " + calc.result());
+    }
 }
